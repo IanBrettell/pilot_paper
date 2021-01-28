53ef35a4d77534831be4495bb1226b1016995d14
brettellebi <brettell@ebi.ac.uk>
2021-01-28 09:55:33 +0000

    20210128 update from local with Dockerfile

--- code/notebooks/20201219_julia_notebook.jl
+++ code/notebooks/20201219_julia_notebook.jl
@@ -1,12 +1,38 @@
-# First header
-```bash
+# Copy data to local
 
+## Activate env
+
+using Pkg
+Pkg.activate("envs/julia")
+Pkg.instantiate()
+
+## Create data directory on local
+if !isdir(joinpath(homedir(), "Documents/Data/20210110_pilot_test_data")
+    mkdir("/Users/brettell/Documents/Data/20210110_pilot_test_data")
+end
+
+## Copy over documents
+```bash
+rclone copy \
+    google_drive_ebi:pilot_paper/idtrackerai/final \
+    ~/Documents/Data/20210110_pilot_test_data
 ```
 
-pwd()
+## Import into list
 
-cd("Documents/Repositories/pilot_paper")
+# Load DataFrames
+using DataFrames, CSV
 
-pwd()
+data_frames = []
+for file in readdir("/Users/brettell/Documents/Data/20210110_pilot_test_data", join = true)
+    println(file)
+end
 
-x = 0
+
+target_file = readdir("/Users/brettell/Documents/Data/20210110_pilot_test_data", join = true)[1]
+
+# Read in test DF
+test_df = DataFrame(CSV.File(target_file))
+
+# Get column names
+plot(test_df[1],)
